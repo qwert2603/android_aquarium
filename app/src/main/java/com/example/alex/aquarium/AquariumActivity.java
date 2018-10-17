5e7f4b2a5514cf399b836250ae7ecbfd26d65158
alex <qwert2603@mail.ru>
2018-10-17 18:15:39 +0700

    * correct fullscreen

--- app/src/main/java/com/example/alex/aquarium/AquariumActivity.java
+++ app/src/main/java/com/example/alex/aquarium/AquariumActivity.java
@@ -24,17 +24,6 @@ public class AquariumActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-        );
-
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-
         super.onCreate(savedInstanceState);
         mAquariumView = new AquariumView(this);
         setContentView(mAquariumView);
@@ -45,6 +34,12 @@ public class AquariumActivity extends Activity {
     public void onDestroy() {
         mAquariumStepNotificationThread.makeStop();
         super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        makeFullscreen();
     }
 
     private class AquariumStepNotificationThread extends Thread {
@@ -72,4 +67,16 @@ public class AquariumActivity extends Activity {
         }
     }
 
+    private void makeFullscreen() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
 }
